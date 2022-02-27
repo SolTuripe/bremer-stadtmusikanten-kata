bad9536cba29b5d63869d3beb2e59eee9de400ed
Sol Turipe <solcis.bio@gmail.com>
2022-02-27 21:28:08 +0100

    Director orders the animals to sing, test

--- src/main/java/com/KataBremen/KataBremen/Animal/Director.java
+++ src/main/java/com/KataBremen/KataBremen/Animal/Director.java
@@ -1,8 +1,19 @@
 package com.KataBremen.KataBremen.Animal;
 
-public class Director extends Animal {
+import java.util.ArrayList;
 
+public class Director {
+    ArrayList<Animal> chorus = new ArrayList();
 
+    public Director(ArrayList<Animal> chorus) {
+        this.chorus = chorus;
+    }
+
+    public void startSinging() {
+        for (Animal animal : chorus) {
+            animal.isSinging();
+        }
+    }
 
 
 }
--- src/test/java/com/KataBremen/KataBremen/AnimalTest/BremenTest.java
+++ src/test/java/com/KataBremen/KataBremen/AnimalTest/BremenTest.java
@@ -1,13 +1,16 @@
 package com.KataBremen.KataBremen.AnimalTest;
 
+import com.KataBremen.KataBremen.Animal.Animal;
 import com.KataBremen.KataBremen.Animal.Cat;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
+import com.KataBremen.KataBremen.Animal.Director;
 import com.KataBremen.KataBremen.Animal.Donkey;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class BremenTest {
 
@@ -72,6 +75,20 @@ public class BremenTest {
 
         donkey.notSing();
         assertThat(donkey.message(), equalTo("The donkey Mr. Bruno isn't singing"));
+    }
+
+    @Test
+    void TheDirectorOrdersToSing() {
+        ArrayList<Animal> chorus = new ArrayList();
+        chorus.add(new Cat("Tobi", "Miau"));
+        chorus.add(new Donkey("Mr. Bruno", "iooooioooo"));
+
+        Director director = new Director(chorus);
+
+        director.startSinging();
+
+        assertEquals("The cat Tobi is singing Miau", chorus.get(0).message());
+        assertEquals("The donkey Mr. Bruno is singing iooooioooo", chorus.get(1).message());
     }
 
 
